cb1b74c046af3a938a218df43b79535bef702561
Abhijith <68745732+abhijithpd01@users.noreply.github.com>
2021-01-12 13:30:04 +0530

    Update and rename README.md to generation.py

---
+++ generation.py
@@ -0,0 +1,13 @@
+# qrcodegeneration
+import qrcode
+qr=qrcode.QRCode(
+  version=1,
+  box_size=10,
+  border=5,
+  )
+
+data="hello"
+qr.add_data(data)
+qr.make(fit=true)
+img=qr.make_image(fill="black",back_color="white")
+img.save("1.png")
